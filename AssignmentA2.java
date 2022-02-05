bcde5eb174cf0c7e421f231cb13bb41a2c60cb1d
Nikhil Jadhav <98797972+nikjadhav97@users.noreply.github.com>
2022-02-05 22:04:50 +0530

    Add files via upload

---
+++ AssignmentA2.java
@@ -0,0 +1,23 @@
+package com.capgemini.java;
+//A2. Print the following patterns on screen with appropriate logic
+
+public class AssignmentA2 {
+	public static void main(String[] args) {
+
+			
+		for (int i=7;i>0;i--) {
+			
+			for (int j=0; j<i;j++) {
+				
+				System.out.println(j+1);
+			}
+			
+			System.out.println("");
+			}
+		}
+	}
+
+		
+		
+
+
---
+++ AssignmentA3.java
@@ -0,0 +1,21 @@
+package com.capgemini.java;
+//A3. Print the following patterns on screen with appropriate logic
+public class AssignmentA3 {
+	public static void main(String[] args) {
+		int i, j, row=6;
+		for(i=0;i<row;i++);
+		{
+			for (j=0; j<=i;j++);
+			
+			{
+				System.out.println("* ");
+				
+				{
+					System.out.println();
+				}
+			}
+		}
+		
+		
+	}
+}
