e2da6bb2d051c2c0dc57e578753c030faf91344e
Nikhil Jadhav <98797972+nikjadhav97@users.noreply.github.com>
2022-02-05 22:04:12 +0530

    Add files via upload

---
+++ AssignmentA1.java
@@ -0,0 +1,29 @@
+package com.capgemini.java;
+//A1. Print the following patterns on screen with appropriate logic
+
+import javax.print.attribute.SetOfIntegerSyntax;
+
+public class AssignmentA1 {
+	public static void main(String[] args) {
+	int i,j;
+		for ( i=1;i<=6;i++) {
+			
+			for( j=1;j<=6;j++) {
+			
+				{
+					System.out.println(j +"  ");
+				}
+				{
+					System.out.println("  "); 
+				}
+			}
+		}
+	}
+}
+		
+		
+			
+		
+	
+
+
